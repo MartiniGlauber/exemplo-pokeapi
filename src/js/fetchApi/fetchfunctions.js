698481e54701c934f359b65ff4deb14a6c7a8ebc
Glauber <gmartinisantos@gmail.com>
2024-10-16 21:35:44 -0300

    iniciando api

--- src/js/fetchApi/fetchfunctions.js
+++ src/js/fetchApi/fetchfunctions.js
@@ -1,15 +1,16 @@
 import { urlPokeApi } from "../constants/constants.js"
+import { showError } from "../errors/errors.js";
+
 
 export async function listAllPokemons(urlApi = urlPokeApi){
-    // try{
+    try{
+        const data = await fetch(urlApi);
+        const response = await data.json();
+        
+        return response;
 
-    //     const data = await fetch(urlApi);
-    //     const json = await data.json();
+    }catch(error){
+        showError("Ops! Erro inesperado");
+    }
 
-    //     console.log("pokemons: ", json);
-    //     return json.results;
-
-    // }catch(error){
-    //     throw Error(error.message)
-    // }
 }
